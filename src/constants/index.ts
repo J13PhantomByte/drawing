416f71d051fe7460ac20d505f1bef15b0224910b
Juan <siapaaakuu05@gmail.com>
2024-12-19 13:07:01 +0700

    drawing

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+// src/constants/index.ts
+export const SWATCHES = [
+    'rgb(255, 0, 0)',
+    'rgb(0, 255, 0)',
+    'rgb(0, 0, 255)',
+    'rgb(255, 255, 255)',
+    'rgb(255, 241, 44)',
+    'rgb(126, 66, 30)',
+    'rgb(194, 78, 181)',
+    'rgb(87, 173, 217)',
+    'rgb(53, 210, 92)',
+    // Tambahkan warna lainnya
+  ];
+  
